e37eb3d91142fa6bfca1244f24d3b777dadc6dc0
Soha Jin <soha@lohu.info>
2019-02-20 02:05:26 +0800

    update key binding

--- src/main/cpp/OI.cpp
+++ src/main/cpp/OI.cpp
@@ -2,6 +2,8 @@
 
 #include "Commands/HamRaisingCommand.h"
 #include "Commands/HamLyingCommand.h"
+#include "Commands/BackHamRaisingCommand.h"
+#include "Commands/BackHamLyingCommand.h"
 #include "Commands/TongueRaisingCommand.h"
 #include "Commands/TongueLyingCommand.h"
 #include "Commands/FootRollerCommand.h"
@@ -16,8 +18,10 @@ OI::OI() {
 	(new frc::JoystickButton(joystickOperator.get(), 9))->WhileHeld(new TongueRaisingCommand());
 	(new frc::JoystickButton(joystickOperator.get(), 10))->WhileHeld(new TongueLyingCommand());
 
-	(new frc::JoystickButton(joystickOperator.get(), 4))->WhileHeld(new HamRaisingCommand());
-	(new frc::JoystickButton(joystickOperator.get(), 2))->WhileHeld(new HamLyingCommand());
+	(new frc::JoystickButton(joystickOperator2.get(), 7))->WhileHeld(new HamRaisingCommand());
+	(new frc::JoystickButton(joystickOperator2.get(), 8))->WhileHeld(new HamLyingCommand());
+	(new frc::JoystickButton(joystickOperator2.get(), 9))->WhileHeld(new BackHamRaisingCommand());
+	(new frc::JoystickButton(joystickOperator2.get(), 10))->WhileHeld(new BackHamLyingCommand());
 	(new frc::JoystickButton(joystickOperator.get(), 1))->WhileHeld(new FootRollerCommand(1)); //push
 	(new frc::JoystickButton(joystickOperator.get(), 3))->WhileHeld(new FootRollerCommand(-0.4));
 	// (new frc::JoystickButton(joystickOperator.get(), 5))->WhileHeld(new FootRollerCommand(-0.05));
