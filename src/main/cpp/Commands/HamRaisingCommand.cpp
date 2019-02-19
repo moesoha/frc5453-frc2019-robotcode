2a21f130f0ba77137b0a07dd8db580e5892e3683
Soha Jin <soha@lohu.info>
2019-02-20 02:13:27 +0800

    test

--- src/main/cpp/Commands/HamRaisingCommand.cpp
+++ src/main/cpp/Commands/HamRaisingCommand.cpp
@@ -14,9 +14,9 @@ void HamRaisingCommand::Execute() {
 
 bool HamRaisingCommand::IsFinished() {
 	// ham 到达一定位置，并且 tongue 的位置不正确的时候强行 finish
-	if(Robot::hamSubsystem->hamDistance()<30 && Robot::tongueSubsystem->position()>120){
-		return true;
-	}
+	// if(Robot::hamSubsystem->hamDistance()<30 && Robot::tongueSubsystem->position()>120){
+	// 	return true;
+	// }
 	return false;
 }
 
